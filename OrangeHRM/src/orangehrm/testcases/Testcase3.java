144df7025ff2046c350a40db17a1b28760223474
Shubh <Shubh@gmail.com>
2023-06-08 17:05:45 +0530

    Test Case 3 inserted

---
+++ OrangeHRM/src/orangehrm/testcases/Testcase3.java
@@ -0,0 +1,10 @@
+package orangehrm.testcases;
+
+public class Testcase3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("This is test Case 3");
+	}
+
+}
