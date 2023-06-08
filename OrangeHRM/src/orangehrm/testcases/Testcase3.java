983eccee78fa2a151d969631c100945fac953444
Shubham Sultane <sultaneshubham1996@gmail.com>
2023-06-08 22:40:50 +0530

    Update Testcase3.java

--- OrangeHRM/src/orangehrm/testcases/Testcase3.java
+++ OrangeHRM/src/orangehrm/testcases/Testcase3.java
@@ -5,6 +5,7 @@ public class Testcase3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("This is test Case 3");
+		System.out.println("This is modified Test Case 3");
 	}
 
 }
