de19f78b09d54e47c15df2978a9131852a58b281
Shubh <Shubh@gmail.com>
2023-06-08 16:44:54 +0530

    commit updated

---
+++ OrangeHRM/src/orangehrm/testcases/TestCase2.java
@@ -0,0 +1,9 @@
+package orangehrm.testcases;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		System.out.println("This is second test case");
+	}
+
+}
---
+++ OrangeHRM/src/orangehrm/testcases/Testcase1.java
@@ -0,0 +1,10 @@
+package orangehrm.testcases;
+
+public class Testcase1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is first Test Case");
+
+	}
+
+}
